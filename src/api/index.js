f3075cf7c03f98f307238b570f1c2f4e4035e34e
ArtOleinikov <aoleinikov@wiley.com>
2025-01-07 18:41:52 +0200

    add API

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,44 @@
-const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YjcxYWMzZjA4OTZlODE1ZjBhZjg0ZjVmMDZlNzYyMCIsIm5iZiI6MTU5MTcwODE0MC42MjMwMDAxLCJzdWIiOiI1ZWRmODllYzljMjRmYzAwMjE2YTk1OWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.OYnKk8TAqHNUguOXyO7cIt8AHyq9p8UzRDN6x8AKk-E'
-const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
+import axios from 'axios'
 
-const options = {
-  headers: {
-    Authorization: `Bearer ${token}`
+const TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YjcxYWMzZjA4OTZlODE1ZjBhZjg0ZjVmMDZlNzYyMCIsIm5iZiI6MTU5MTcwODE0MC42MjMwMDAxLCJzdWIiOiI1ZWRmODllYzljMjRmYzAwMjE2YTk1OWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.OYnKk8TAqHNUguOXyO7cIt8AHyq9p8UzRDN6x8AKk-E'
+const BASE_URL = 'https://api.themoviedb.org/3/'
+
+class Api {
+  static getTrendingMovies() {
+    return this.get(`trending/movie/day?language=en-US`)
   }
-};
-  
+
+  static searchMovies(query, page = 1) {
+    return this.get(`/search/movie?query=${query}&include_adult=false&language=en-US&page=${page}`)
+  }
+
+  static getMovieDetails(movie_id) {
+    return this.get(`/movie/${movie_id}?language=en-US`)
+  }
+
+  static getMovieCredits(movie_id) {
+    return this.get(`/movie/${movie_id}/credits`)
+  }
+
+  static getMovieReviews(movie_id) {
+    return this.get(`/movie/${movie_id}/reviews`)
+  }
+
+  static async get(url) {
+    try {
+      const response = await axios.request({
+        url: `${BASE_URL}${url}`,
+        headers: { 
+          Authorization: `Bearer ${TOKEN}`,
+          accept: 'application/json',
+        }
+      })
+      return response.data
+    } catch (error) {
+      console.error(error)
+      return null
+    }
+  }
+}
+
+export default Api
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import Api from '../../api'
+
 function HomePage() {
-  return <main>Home Page</main>
+  const [trendingMovies, setTrendingMovies] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await Api.getTrendingMovies()
+      console.log(data)
+      setTrendingMovies(data.results)
+    }
+    getData()
+  }, [])
+
+  return (
+    <main>
+      <h1>Trending today</h1>
+      <ul>
+        {trendingMovies.map(movie => (
+          <li key={movie.id}>
+            <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
 }
 
 export default HomePage
